d42432b82cd73af2a4cc33fdb4d5640788d34bac
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-09 18:42:11 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 # Отримуємо змінні середовища
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Додано
+WEBHOOK_URL = os.getenv("https://git.heroku.com/mlbb.git")  # Додано
 
 # Замінюємо 'postgres://' на 'postgresql://', якщо необхідно
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
